11568a30ff8a9edfe6040443657f819315904453
DaffaRafsanjani <101723280+DaffaRafsanjani@users.noreply.github.com>
2022-03-24 11:34:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi = new ConsoleApplication2.taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
